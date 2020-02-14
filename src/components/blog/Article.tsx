0feb64a5b88b66bc4f9e0fffa240767fafbe04dd
Francois Best <francois@francoisbest.com>
2020-02-14 16:11:07 +0100

    feat: Add SEO for blog articles

--- src/components/blog/Article.tsx
+++ src/components/blog/Article.tsx
@@ -7,6 +7,7 @@ import Mdx from './Mdx'
 import blogTheme from './theme'
 import Head from 'next/head'
 import ArticleFooter from './Footer'
+import { NextSeo } from 'next-seo'
 
 export interface ArticleProps {
   meta: ArticleMeta
@@ -37,6 +38,26 @@ const Article: React.SFC<ArticleProps> = ({ meta, children, ...props }) => {
           rel="stylesheet"
         />
       </Head>
+      <NextSeo
+        title={meta.title}
+        description="A blog article by François Best"
+        additionalMetaTags={[{ property: 'author', content: 'François Best' }]}
+        twitter={{
+          cardType: 'summary',
+          handle: 'fortysevenfx',
+          site: 'fortysevenfx'
+        }}
+        openGraph={{
+          type: 'article',
+          title: meta.title,
+          article: {
+            publishedTime: meta.publicationDate,
+            modifiedTime: meta.publicationDate,
+            authors: ['https://francoisbest.com'],
+            tags: meta.tags
+          }
+        }}
+      />
       <Box as="article" maxW="2xl" mx="auto" px={[3, 3, 1]} mt={12}>
         <ArticleHeader meta={meta} readingTime={readingTime} mb={12} />
         <ThemeProvider theme={blogTheme}>
--- src/components/blog/types.ts
+++ src/components/blog/types.ts
@@ -5,7 +5,7 @@ export interface AlsoPublishedOn {
 
 export interface ArticleMeta {
   title: string
-  publicationDate?: Date // null for drafts
+  publicationDate?: string
   preview?: () => JSX.Element
   tags?: string[]
   alsoPublishedOn?: AlsoPublishedOn[]
