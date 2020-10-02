18d2cc60717f5a035ca688b422b4d860a0661f69
Francois Best <francois@francoisbest.com>
2020-10-02 17:02:05 +0200

    feat: Add simple E2EE test page

---
+++ src/pages/e2ee/[payload].tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import * as e2ee from '@47ng/simple-e2ee'
+import { ButtonRouteLink, StackContainer } from '@47ng/chakra-next'
+import {
+  Button,
+  Input,
+  Stack,
+  Text,
+  Textarea,
+  useClipboard
+} from '@chakra-ui/core'
+
+export interface E2EEProps {}
+
+const E2EE: NextPage<E2EEProps> = ({}) => {
+  const router = useRouter()
+  const [text, setText] = React.useState('')
+  const url = typeof window === 'undefined' ? '' : window.location.toString()
+  const { onCopy, hasCopied } = useClipboard(url)
+  React.useEffect(() => {
+    const key = e2ee.getKeyFromURL()
+    const { payload } = router.query as { [key: string]: string }
+    if (!payload) {
+      // First hydration
+      return
+    }
+    setText(e2ee.decrypt<string>(payload, key))
+  }, [router])
+
+  return (
+    <StackContainer mt={8}>
+      <Text>Share the URL anywhere you want </Text>
+      <Stack isInline>
+        <Input isReadOnly value={url} size="sm" />
+        <Button onClick={onCopy} ml={2} size="sm">
+          {hasCopied ? 'Copied' : 'Copy'}
+        </Button>
+      </Stack>
+
+      <Text mt={8}>Decrypted data:</Text>
+      <Textarea isReadOnly value={text} minH="200px" />
+      <ButtonRouteLink to="/e2ee/encrypt" mt={4}>
+        Encrypt new message
+      </ButtonRouteLink>
+    </StackContainer>
+  )
+}
+
+export default E2EE
---
+++ src/pages/e2ee/encrypt.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { NextPage } from 'next'
+import * as e2ee from '@47ng/simple-e2ee'
+import { StackContainer } from '@47ng/chakra-next'
+import { Button, Textarea } from '@chakra-ui/core'
+import { useRouter } from 'next/router'
+
+export interface E2EEProps {}
+
+const E2EE: NextPage<E2EEProps> = ({}) => {
+  const [text, setText] = React.useState('')
+  const router = useRouter()
+  const encrypt = React.useCallback(() => {
+    const { key, payload } = e2ee.encrypt(text)
+    const url = new URL(
+      e2ee.applyKeyToURL(
+        key,
+        window.location.toString().replace(/\/encrypt$/, `/${payload}`)
+      )
+    )
+    router.push(url)
+  }, [text])
+
+  return (
+    <StackContainer mt={8}>
+      <Textarea
+        value={text}
+        onChange={(e: any) => setText(e.target.value)}
+        placeholder="Message to encrypt (max 256 characters)"
+        maxLength={256}
+        minH="200px"
+      />
+      <Button variantColor="green" onClick={encrypt}>
+        Encrypt
+      </Button>
+    </StackContainer>
+  )
+}
+
+export default E2EE
