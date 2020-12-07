71ab6cf48529db3275e3a3846bc3fe3f6e89a9a2
Francois Best <francois@francoisbest.com>
2020-12-07 16:39:31 +0100

    chore: Codemod, variantColor => colorScheme

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -4,20 +4,20 @@ import { useColor } from 'src/ui/colors'
 import { ColorKeys, CustomTheme } from 'src/ui/theme'
 
 export interface BadgeProps extends BoxProps {
-  variantColor?: ColorKeys
+  colorScheme?: ColorKeys
 }
 
 export const Badge: React.FC<BadgeProps> = ({
-  variantColor = 'accent',
+  colorScheme = 'accent',
   ...props
 }) => {
   const theme = useTheme() as CustomTheme
   const colorProps = {
     bg:
-      variantColor === 'accent'
+      colorScheme === 'accent'
         ? useColor('accent.100', 'var(--colors-badge-bg-dark)')
-        : useColor(`${variantColor}.100`, theme.badgeBgDark[variantColor]),
-    color: useColor(`${variantColor}.800`, `${variantColor}.200`)
+        : useColor(`${colorScheme}.100`, theme.badgeBgDark[colorScheme]),
+    color: useColor(`${colorScheme}.800`, `${colorScheme}.200`)
   } as const
   return (
     <Box
