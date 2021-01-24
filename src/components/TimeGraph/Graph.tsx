d6c826300dc5110be973a0dbf74e1047c624d691
Francois Best <francois@francoisbest.com>
2021-01-24 01:52:29 +0100

    fix: Remove unused imports

--- src/components/TimeGraph/Graph.tsx
+++ src/components/TimeGraph/Graph.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { LinearGradient } from '@visx/gradient'
-import { useToken } from '@chakra-ui/react'
 import { Svg, SvgProps } from '@47ng/chakra-next'
 
 // --
@@ -9,8 +7,6 @@ export interface GraphProps extends SvgProps {
   w: number
   h?: number
 }
-
-const useColor = (token: string) => useToken('colors', token)
 
 export const Graph: React.FC<GraphProps> = ({
   w,
@@ -26,12 +22,6 @@ export const Graph: React.FC<GraphProps> = ({
       // role="graphics-object"
       {...props}
     >
-      {/* <LinearGradient
-        from={useColor('gray.900')}
-        to={useColor('accent.900')}
-        id="bg-gradient"
-      />
-      <rect width={w} height={h} fill="url(#bg-gradient)" /> */}
       {children}
     </Svg>
   )
