dde8530a95dc7fcea6ecfbf0953baca0493b8fa7
Francois Best <francois@francoisbest.com>
2021-02-26 22:17:31 +0100

    chore: Automatic layout
    Detect which layout to use based on the path, with overrides.

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,3 +1,36 @@
+import React from 'react'
 import PostLayout from './PostLayout'
+import PageLayout from './PageLayout'
+import RawLayout from './RawLayout'
+import { ExtendedMDXFrontMatter } from 'src/types'
 
-export default PostLayout
+const layouts = {
+  PostLayout,
+  PageLayout,
+  RawLayout
+}
+
+function useLayoutComponent({ layout, path }: ExtendedMDXFrontMatter) {
+  if (layout && layout in layouts) {
+    return layouts[layout as keyof typeof layouts]
+  }
+  if (path.startsWith('/posts/')) {
+    return PostLayout
+  }
+  return PageLayout
+}
+
+export function AutomaticLayout({
+  children,
+  frontMatter,
+  ...props
+}: React.PropsWithChildren<{ frontMatter: any }>) {
+  const Layout = useLayoutComponent(frontMatter)
+  return (
+    <Layout frontMatter={frontMatter} {...props}>
+      {children}
+    </Layout>
+  )
+}
+
+export default AutomaticLayout
