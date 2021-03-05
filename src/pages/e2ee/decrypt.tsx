a6ca5a93009e62d20a2df9f795ae2131c174849b
Francois Best <francois@francoisbest.com>
2021-03-05 21:45:40 +0100

    feat: Add complete UI for the E2EE toy

--- src/pages/e2ee/decrypt.tsx
+++ src/pages/e2ee/decrypt.tsx
@@ -1,51 +1,222 @@
-import React from 'react'
-import { NextPage } from 'next'
-import { useRouter } from 'next/router'
+import { RouteLink, RouteLinkProps } from '@47ng/chakra-next'
 import * as e2ee from '@47ng/simple-e2ee'
-import { ButtonRouteLink } from '@47ng/chakra-next'
 import {
   Button,
-  Container,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Icon,
   Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Stack,
   Text,
   Textarea,
   useClipboard
 } from '@chakra-ui/react'
+import { NextPage } from 'next'
+import { useQueryState } from 'next-usequerystate'
+import React from 'react'
+import { FiCheck, FiCopy } from 'react-icons/fi'
+import { IoKeyOutline } from 'react-icons/io5'
+import { H1, Paragraph } from 'src/components/primitives/Typography'
+import { useURL } from 'src/hooks/useURL'
+import PageLayoutWithSEO from 'src/layouts/PageLayout'
+import { useLinkColor } from 'src/ui/theme'
 
-export interface E2EEProps {}
-
-const E2EE: NextPage<E2EEProps> = ({}) => {
-  const router = useRouter()
-  const [text, setText] = React.useState('')
-  const url = typeof window === 'undefined' ? '' : window.location.toString()
-  const { onCopy, hasCopied } = useClipboard(url)
-  React.useEffect(() => {
-    const key = e2ee.getKeyFromURL()
-    const { payload } = router.query as { [key: string]: string }
-    if (!payload) {
-      // First hydration
-      return
+function useHashState() {
+  const [value, _setValue] = React.useState(() => {
+    if (typeof window === 'undefined') {
+      return '' // No access to hash on SSR/SSG
+    } else {
+      return window.location.hash.replace(/^#/, '')
     }
-    setText(e2ee.decrypt<string>(payload, key))
-  }, [router])
+  })
+  const setValue = React.useCallback((v: string) => {
+    window.location.hash = v
+  }, [])
+  React.useEffect(() => {
+    const onHashChange = () => {
+      _setValue(window.location.hash.replace(/^#/, ''))
+    }
+    window.addEventListener('hashchange', onHashChange)
+    return () => {
+      window.removeEventListener('hashchange', onHashChange)
+    }
+  }, [])
+  return [value, setValue] as const
+}
+
+// --
+
+function useDecryption(
+  payload: string | null,
+  key?: string
+): { secret?: string; error?: string } {
+  if (!payload) {
+    return {
+      secret: undefined,
+      error: 'Missing payload'
+    }
+  }
+  if (!key) {
+    return {
+      secret: undefined,
+      error: 'Missing key'
+    }
+  }
+  try {
+    const secret = e2ee.decrypt<string>(payload, key)
+    return { secret }
+  } catch (error) {
+    return {
+      error: (error.message as string)
+        .split('')
+        .map((c, i) => (i === 0 ? c.toUpperCase() : c))
+        .join('')
+    }
+  }
+}
+
+// --
+
+const NewSecretLink: React.FC<Omit<RouteLinkProps, 'to'>> = ({ ...props }) => (
+  <RouteLink
+    to="/e2ee/encrypt"
+    mt={4}
+    color={useLinkColor()}
+    fontWeight="medium"
+    {...props}
+  >
+    Encrypt new secret
+  </RouteLink>
+)
+
+// --
+
+const E2EE: NextPage = ({}) => {
+  const [payload] = useQueryState('payload')
+  const [key, setKey] = useHashState()
+  const [askForKey, setAskForKey] = React.useState(false)
+  const { secret, error } = useDecryption(payload, key)
+  const { onCopy, hasCopied } = useClipboard(secret ?? '')
+
+  React.useEffect(() => {
+    if (secret !== undefined) {
+      setAskForKey(false)
+    }
+  }, [secret])
+
+  React.useLayoutEffect(() => {
+    setAskForKey(!!payload && !key)
+  }, [])
+
+  const focusRef = React.useRef<any>()
 
   return (
-    <Stack as={Container} mt={8}>
-      <Text>Share the URL anywhere you want </Text>
-      <Stack isInline>
-        <Input isReadOnly value={url} size="sm" />
-        <Button onClick={onCopy} ml={2} size="sm">
-          {hasCopied ? 'Copied' : 'Copy'}
-        </Button>
-      </Stack>
-
-      <Text mt={8}>Decrypted data:</Text>
-      <Textarea isReadOnly value={text} minH="200px" />
-      <ButtonRouteLink to="/e2ee/encrypt" mt={4}>
-        Encrypt new message
-      </ButtonRouteLink>
-    </Stack>
+    <PageLayoutWithSEO
+      frontMatter={{
+        title: 'Decrypt Shared Secret',
+        description: 'Decrypt a shared secret stored in the URL',
+        path: '/e2ee/decrypt',
+        url: useURL('/e2ee/decrypt'),
+        ogImage: {
+          url: useURL('/images/e2ee/decrypt/og.jpg'),
+          width: 1280,
+          height: 720
+        }
+      }}
+    >
+      <H1>Decrypt Shared Secret</H1>
+      {error === 'Missing payload' ? (
+        <Stack mt={12} spacing={8}>
+          <Text>
+            This page requires an encrypted payload in the URL to decrypt.
+          </Text>
+          <NewSecretLink />
+        </Stack>
+      ) : (
+        <Text color="red.600" display={askForKey || !error ? 'none' : 'block'}>
+          Error: {error}
+        </Text>
+      )}
+      {secret !== undefined && (
+        <Stack>
+          <FormControl>
+            <FormLabel
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              marginInlineEnd={0}
+            >
+              <Text>Decrypted secret</Text>
+              <Button
+                size="xs"
+                onClick={onCopy}
+                variant="ghost"
+                colorScheme="accent"
+                leftIcon={hasCopied ? <FiCheck /> : <FiCopy />}
+                mr={0}
+              >
+                Copy
+              </Button>
+            </FormLabel>
+            <Textarea
+              value={secret}
+              onChange={() => {}}
+              minH="200px"
+              placeholder="Hello darkness my old friend (your secret is an empty string)."
+            />
+          </FormControl>
+          <NewSecretLink />
+        </Stack>
+      )}
+      <Modal
+        isOpen={askForKey}
+        onClose={() => setAskForKey(false)}
+        closeOnEsc={false}
+        closeOnOverlayClick={false}
+        isCentered
+        initialFocusRef={focusRef}
+        size="xl"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            <Icon as={IoKeyOutline} mt={-1} mr={1} /> Enter key
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Paragraph>
+              This encrypted secret URL requires a key to be decrypted. Whoever
+              sent it to you should have sent you the key using a separate
+              channel.
+            </Paragraph>
+            <FormControl>
+              <FormLabel>Paste the key here:</FormLabel>
+              <Input
+                ref={focusRef}
+                value={key}
+                onChange={e => setKey(e.target.value)}
+              />
+              <FormHelperText color="red.600">
+                {error && error !== 'Missing key' ? (
+                  <>Error: {error}</>
+                ) : (
+                  <>&nbsp;</>
+                )}
+              </FormHelperText>
+            </FormControl>
+          </ModalBody>
+          <ModalFooter />
+        </ModalContent>
+      </Modal>
+    </PageLayoutWithSEO>
   )
 }
 
--- src/pages/e2ee/encrypt.tsx
+++ src/pages/e2ee/encrypt.tsx
@@ -1,41 +1,293 @@
-import React from 'react'
-import { NextPage } from 'next'
+import { OutgoingLink, RouteLink } from '@47ng/chakra-next'
 import * as e2ee from '@47ng/simple-e2ee'
-import { Button, Textarea, Stack, Container } from '@chakra-ui/react'
-import { useRouter } from 'next/router'
+import {
+  Box,
+  BoxProps,
+  Button,
+  Center,
+  chakra,
+  Checkbox,
+  CircularProgress,
+  CircularProgressLabel,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Icon,
+  Input,
+  InputGroup,
+  InputProps,
+  InputRightElement,
+  Stack,
+  Textarea,
+  useClipboard,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { NextPage } from 'next'
+import React from 'react'
+import { FiAlertTriangle, FiCheck, FiCopy } from 'react-icons/fi'
+// @ts-ignore
+import HowItWorks from 'src/../public/images/e2ee/encrypt/how-it-works.svg'
+import { mdxComponents } from 'src/components/blog/Mdx'
+import { RepoReference } from 'src/components/blog/RepoReference'
+import { H1, H3, H5, Paragraph } from 'src/components/primitives/Typography'
+import { useURL } from 'src/hooks/useURL'
+import PageLayoutWithSEO from 'src/layouts/PageLayout'
+import { useLinkColor } from 'src/ui/theme'
 
 export interface E2EEProps {}
 
+const MAX_INPUT_LENGTH = 1024
+
 const E2EE: NextPage<E2EEProps> = ({}) => {
   const [text, setText] = React.useState('')
-  const router = useRouter()
-  const encrypt = React.useCallback(() => {
-    const { key, payload } = e2ee.encrypt(text)
-    const url = new URL(
-      e2ee.applyKeyToURL(
-        key,
-        window.location
-          .toString()
-          .replace(/\/encrypt$/, `/decrypt?payload=${payload}`)
-      )
-    )
-    router.push(url)
-  }, [text])
+  const [secure, setSecure] = React.useState(false)
+  const { payload, key } = React.useMemo(() => e2ee.encrypt(text), [text])
+
+  const url = React.useMemo(() => {
+    const baseURL = useURL('/e2ee/decrypt') + `?payload=${payload}`
+    if (secure) {
+      return e2ee.applyKeyToURL(key, baseURL)
+    } else {
+      return baseURL
+    }
+  }, [payload, key, secure])
 
   return (
-    <Stack as={Container} mt={8}>
-      <Textarea
-        value={text}
-        onChange={(e: any) => setText(e.target.value)}
-        placeholder="Message to encrypt (max 256 characters)"
-        maxLength={256}
-        minH="200px"
-      />
-      <Button colorScheme="green" onClick={encrypt}>
-        Encrypt
-      </Button>
-    </Stack>
+    <PageLayoutWithSEO
+      frontMatter={{
+        title: 'Share Encrypted Secret',
+        description: 'Encrypt and share a secret without using a server',
+        path: '/e2ee/encrypt',
+        url: useURL('/e2ee/encrypt'),
+        ogImage: {
+          url: useURL('/images/e2ee/encrypt/og.jpg'),
+          width: 1280,
+          height: 720
+        }
+      }}
+    >
+      <H1>Share Encrypted Secret</H1>
+      <Paragraph>
+        Encrypt and share a secret. Everything is stored in the URL, nothing is
+        stored or sent anywhere.
+      </Paragraph>
+      <Stack mt={8}>
+        <Box pos="relative">
+          <Textarea
+            value={text}
+            onChange={(e: any) => setText(e.target.value)}
+            placeholder={`Message to encrypt (max ${MAX_INPUT_LENGTH} characters)`}
+            maxLength={MAX_INPUT_LENGTH}
+            minH="200px"
+          />
+          <CharacterCountProgress
+            count={text.length}
+            maxCount={MAX_INPUT_LENGTH}
+            pos="absolute"
+            right={3}
+            bottom={3}
+          />
+        </Box>
+        <FormControl mb={8}>
+          <Checkbox
+            isChecked={secure}
+            onChange={e => setSecure(e.target.checked)}
+          >
+            Share over secure channel
+          </Checkbox>
+          <FormHelperText fontSize="xs">
+            Signal, Keybase, WhatsApp,{' '}
+            <OutgoingLink href="https://ephemere.app">Ephemere</OutgoingLink>,
+            anything end-to-end encrypted.
+          </FormHelperText>
+        </FormControl>
+      </Stack>
+      <Stack spacing={4} mt={4}>
+        <FormControl>
+          <FormLabel>Share this URL</FormLabel>
+          <ReadOnlyInputWithCopy value={url} fontSize="sm" />
+          {secure && (
+            <FormHelperText fontSize="xs">
+              <Icon
+                as={FiAlertTriangle}
+                verticalAlign="top"
+                mr={1}
+                mt="1px"
+                strokeWidth={2.5}
+              />
+              <chakra.b fontWeight="medium">Warning</chakra.b>: it contains the
+              key. Read more below.
+            </FormHelperText>
+          )}
+        </FormControl>
+        {!secure && (
+          <FormControl>
+            <FormLabel>Share the key on a separate channel</FormLabel>
+            <ReadOnlyInputWithCopy
+              value={key}
+              fontFamily="mono"
+              fontSize="sm"
+            />
+          </FormControl>
+        )}
+      </Stack>
+      <H3 linkable id="how-does-it-work" mt={12}>
+        How does it work ?
+      </H3>
+      <Paragraph>
+        Your secret is encrypted using{' '}
+        <OutgoingLink
+          href="https://tweetnacl.js.org/#/secretbox"
+          color={useLinkColor()}
+        >
+          TweetNaCl
+        </OutgoingLink>
+        , with a{' '}
+        <OutgoingLink href="https://tweetnacl.js.org/#/secretbox">
+          <mdxComponents.inlineCode
+            color={useLinkColor()}
+            _hover={{ textDecoration: 'underline' }}
+          >
+            secret_box
+          </mdxComponents.inlineCode>
+        </OutgoingLink>{' '}
+        cipher. The ciphertext (and nonce) is stored in the URL as a query
+        string (which is public).
+      </Paragraph>
+      <Paragraph>
+        How the key is handled depends on how secure your channel of
+        communication is. If using a secure channel, the key can be stored in
+        the URL hash, which never reaches my server. It makes it more convenient
+        as there is only one thing to share:
+      </Paragraph>
+      <Paragraph as={Center}>
+        <HowItWorks
+          className="darkModeInvertLuminosity"
+          style={{ maxWidth: '500px' }}
+        />
+      </Paragraph>
+      <Paragraph>
+        However, for unsecure channels (unencrypted chats like Messenger, Slack,
+        email, etc), the key should travel separately. It's best to use a
+        completely different method of communication to send it, eg: URL over
+        Slack, key over Messenger.
+      </Paragraph>
+      <H3>What if...</H3>
+      <H5>We used a password to encrypt the key ?</H5>
+      <Paragraph>
+        Instead of sharing the key separately, we could encrypt it with a
+        password, and ask for the password instead. But since humans are
+        terrible at generating passwords, we might as well use the key directly
+        as it's guaranteed to have more entropy.
+      </Paragraph>
+      <H5>We want an expiring link ?</H5>
+      <Paragraph>
+        This unfortunately requires some statefulness. If storing the ciphertext
+        is not desired (larger size), the key could be split using{' '}
+        <RouteLink to="/horcrux" color={useLinkColor()}>
+          Shamir Secret Sharing
+        </RouteLink>
+        . One fragment would be shared in the URL hash, and the other sent to a
+        Redis instance (or any auto-expiring KV store).
+      </Paragraph>
+      <H3>Details &amp; Notes</H3>
+      <Paragraph>
+        This is abstracted in a little TypeScript package I made:
+      </Paragraph>
+      <RepoReference {...require('github:47ng/simple-e2ee')} />
+      <Paragraph>
+        Because a URL can only store so much data, your secret must be short
+        (max {MAX_INPUT_LENGTH} characters, as the ciphertext + key will be much
+        longer).
+      </Paragraph>
+      <Paragraph>
+        If you liked this little encryption toy, check out{' '}
+        <b>
+          <RouteLink to="/horcrux" color={useLinkColor()}>
+            Horcrux
+          </RouteLink>
+        </b>
+        , a playground for splitting secrets with{' '}
+        <OutgoingLink
+          href="https://en.wikipedia.org/wiki/Shamir%27s_Secret_Sharing"
+          color={useLinkColor()}
+        >
+          Shamir Secret Sharing
+        </OutgoingLink>
+        .
+      </Paragraph>
+    </PageLayoutWithSEO>
   )
 }
 
 export default E2EE
+
+// --
+
+export interface CharacterCountProgressProps extends BoxProps {
+  count: number
+  maxCount: number
+}
+
+export const CharacterCountProgress: React.FC<CharacterCountProgressProps> = ({
+  count,
+  maxCount,
+  ...props
+}) => {
+  if (count === 0) {
+    return null
+  }
+  const progress = Math.round((count / maxCount) * 100)
+  const remaining = maxCount - count
+  const isLow = remaining <= 20
+  const color = isLow ? 'orange.500' : 'green.500'
+  return (
+    <Box {...props}>
+      <CircularProgress
+        value={progress}
+        size={6}
+        thickness={12}
+        transform={isLow ? 'scale(1.2)' : 'none'}
+        color={color}
+        trackColor={useColorModeValue('gray.300', 'gray.800')}
+      >
+        <CircularProgressLabel
+          fontSize="xx-small"
+          color={color}
+          visibility={isLow ? 'visible' : 'hidden'}
+        >
+          {remaining}
+        </CircularProgressLabel>
+      </CircularProgress>
+    </Box>
+  )
+}
+
+// --
+
+export interface ReadOnlyInputWithCopyProps extends InputProps {
+  value: string
+}
+
+export const ReadOnlyInputWithCopy: React.FC<ReadOnlyInputWithCopyProps> = ({
+  value,
+  ...props
+}) => {
+  const { onCopy, hasCopied } = useClipboard(value)
+  return (
+    <InputGroup>
+      <Input isReadOnly value={value} pr="5.5rem" {...props} />
+      <InputRightElement w="5.5rem">
+        <Button
+          size="sm"
+          variant="ghost"
+          colorScheme="accent"
+          onClick={onCopy}
+          leftIcon={hasCopied ? <FiCheck /> : <FiCopy />}
+        >
+          Copy
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+  )
+}
