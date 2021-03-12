22e44682cc49ab51b8372cff0694ab206cb46b71
Francois Best <francois@francoisbest.com>
2021-03-12 09:17:56 +0100

    chore: Pull out variable hooks into own components

--- src/components/blog/BlogIndex.tsx
+++ src/components/blog/BlogIndex.tsx
@@ -1,6 +1,7 @@
 import { OutgoingLink } from '@47ng/chakra-next'
 import {
   Box,
+  ChakraProps,
   CloseButton,
   Flex,
   Input,
@@ -8,6 +9,7 @@ import {
   InputLeftElement,
   InputRightElement,
   Stack,
+  StackProps,
   Text,
   useColorModeValue
 } from '@chakra-ui/react'
@@ -110,52 +112,75 @@ export const BlogIndex: React.FC<BlogIndexProps> = ({ posts }) => {
           />
         )}
       </InputGroup>
-      {tag && (
-        <Stack
-          isInline
-          alignItems="center"
-          bg={useColorModeValue('blue.50', 'gray.800')}
-          p={2}
-          pl={3}
-          spacing={3}
-          rounded="md"
-          borderWidth="1px"
-          borderColor={useColorModeValue('blue.200', 'blue.800')}
-          color={useColorModeValue('blue.900', 'blue.300')}
-          fontSize="sm"
-          css={useAccentStyles('blue')}
-        >
-          <Box
-            as={FiTag}
-            role="img"
-            aria-label="Info"
-            color={useColorModeValue('blue.600', 'blue.400')}
-            w={4}
-            h={4}
-          />
-          <Text>Showing posts tagged</Text>
-          <Tag name={tag} mt="1px" interactive={false} mr="auto" />
-          <CloseButton
-            rounded="full"
-            aria-label="Clear"
-            size="sm"
-            onClick={() => setTag(null)}
-          />
-        </Stack>
-      )}
+      {tag && <TagInfoBox tag={tag} onClear={() => setTag(null)} />}
       {filteredPosts.map(post => (
         <PostPreview key={post.url} frontMatter={post} mb={8} />
       ))}
-      {filteredPosts.length === 0 && (
-        <Paragraph
-          textAlign="center"
-          my={12}
-          fontSize="sm"
-          color={useColorModeValue('gray.600', 'gray.500')}
-        >
-          No posts found.
-        </Paragraph>
-      )}
+      {filteredPosts.length === 0 && <NoResults />}
     </>
+  )
+}
+
+// --
+
+export interface TagInfoBoxProps extends StackProps {
+  tag: string
+  onClear: () => void
+}
+
+export const TagInfoBox: React.FC<TagInfoBoxProps> = ({
+  tag,
+  onClear,
+  ...props
+}) => {
+  return (
+    <Stack
+      isInline
+      alignItems="center"
+      bg={useColorModeValue('blue.50', 'gray.800')}
+      p={2}
+      pl={3}
+      spacing={3}
+      rounded="md"
+      borderWidth="1px"
+      borderColor={useColorModeValue('blue.200', 'blue.800')}
+      color={useColorModeValue('blue.900', 'blue.300')}
+      fontSize="sm"
+      css={useAccentStyles('blue')}
+      {...props}
+    >
+      <Box
+        as={FiTag}
+        role="img"
+        aria-label="Info"
+        color={useColorModeValue('blue.600', 'blue.400')}
+        w={4}
+        h={4}
+      />
+      <Text>Showing posts tagged</Text>
+      <Tag name={tag} mt="1px" interactive={false} mr="auto" />
+      <CloseButton
+        rounded="full"
+        aria-label="Clear"
+        size="sm"
+        onClick={onClear}
+      />
+    </Stack>
+  )
+}
+
+// --
+
+export const NoResults: React.FC<ChakraProps> = ({ ...props }) => {
+  return (
+    <Paragraph
+      textAlign="center"
+      my={12}
+      fontSize="sm"
+      color={useColorModeValue('gray.600', 'gray.500')}
+      {...props}
+    >
+      No posts found.
+    </Paragraph>
   )
 }
