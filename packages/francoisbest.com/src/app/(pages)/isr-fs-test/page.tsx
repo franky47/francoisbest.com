9bb25287e9d79c4fa0f0c0d3942c93b5ae2bb59e
Francois Best <github@francoisbest.com>
2023-09-16 12:52:46 +0200

    chore: Try going through import.meta.url

--- packages/francoisbest.com/src/app/(pages)/isr-fs-test/page.tsx
+++ packages/francoisbest.com/src/app/(pages)/isr-fs-test/page.tsx
@@ -1,3 +1,4 @@
+import { nextJsRootDir, resolve } from 'lib/paths'
 import fs from 'node:fs/promises'
 import path from 'node:path'
 
@@ -8,11 +9,13 @@ export const revalidate = 60
 
 export default async function ISRFSTestPage() {
   const now = new Date().toISOString().slice(11, 16)
-  console.log(`[ISR-FS-TEST] process.cwd(): ${process.cwd()}`)
   console.log(`[ISR-FS-TEST] Now: ${now}`)
+  console.log(`[ISR-FS-TEST] process.cwd(): ${process.cwd()}`)
+  const absPath = resolve(import.meta.url, './file.txt')
+  console.log(`[ISR-FS-TEST] Abs path: ${absPath}`)
   const filePath = path.resolve(
     process.cwd(),
-    './src/app/(pages)/isr-fs-test/file.txt'
+    absPath.replace(nextJsRootDir, '.')
   )
   console.log(`[ISR-FS-TEST] File path: ${filePath}`)
   const contents = await fs.readFile(filePath, 'utf-8')
--- packages/francoisbest.com/src/lib/paths.ts
+++ packages/francoisbest.com/src/lib/paths.ts
@@ -2,7 +2,7 @@ import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
-const nextJsRootDir = path.resolve(__dirname, '../../')
+export const nextJsRootDir = path.resolve(__dirname, '../../')
 
 export const repoRoot = path.resolve(nextJsRootDir, '../../')
 export const nextJsAppDir = path.resolve(nextJsRootDir, 'src', 'app')
