60be243855ec4455c2c165eeb29500f6dc42f253
Francois Best <github@francoisbest.com>
2023-09-16 12:44:47 +0200

    chore: Test recommended way of accessing paths

--- packages/francoisbest.com/src/app/(pages)/isr-fs-test/page.tsx
+++ packages/francoisbest.com/src/app/(pages)/isr-fs-test/page.tsx
@@ -1,5 +1,5 @@
-import { resolve } from 'lib/paths'
 import fs from 'node:fs/promises'
+import path from 'node:path'
 
 export const metadata = {
   title: 'ISR Filesystem test',
@@ -9,7 +9,10 @@ export const revalidate = 60
 export default async function ISRFSTestPage() {
   const now = new Date().toISOString().slice(11, 16)
   console.log(`[ISR-FS-TEST] Now: ${now}`)
-  const filePath = resolve(import.meta.url, './file.txt')
+  const filePath = path.resolve(
+    process.cwd(),
+    './packages/francoisbest.com/src/app/(pages)/isr-fs-test/file.txt'
+  )
   console.log(`[ISR-FS-TEST] File path: ${filePath}`)
   const contents = await fs.readFile(filePath, 'utf-8')
   console.log(`[ISR-FS-TEST] Read: ${contents}`)
