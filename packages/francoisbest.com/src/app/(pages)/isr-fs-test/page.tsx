a89856a520ba72b0f596294133aeabf25b7a989b
Francois Best <github@francoisbest.com>
2023-09-16 12:46:46 +0200

    chore: CWD is next package root

--- packages/francoisbest.com/src/app/(pages)/isr-fs-test/page.tsx
+++ packages/francoisbest.com/src/app/(pages)/isr-fs-test/page.tsx
@@ -12,7 +12,7 @@ export default async function ISRFSTestPage() {
   console.log(`[ISR-FS-TEST] Now: ${now}`)
   const filePath = path.resolve(
     process.cwd(),
-    './packages/francoisbest.com/src/app/(pages)/isr-fs-test/file.txt'
+    './src/app/(pages)/isr-fs-test/file.txt'
   )
   console.log(`[ISR-FS-TEST] File path: ${filePath}`)
   const contents = await fs.readFile(filePath, 'utf-8')
