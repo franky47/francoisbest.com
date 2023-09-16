3ad21e5e0f9a6e04cc6d54c061dfa2332ba43057
Francois Best <github@francoisbest.com>
2023-09-16 12:53:26 +0200

    chore: Debug printout

--- packages/francoisbest.com/src/app/(pages)/isr-fs-test/page.tsx
+++ packages/francoisbest.com/src/app/(pages)/isr-fs-test/page.tsx
@@ -10,6 +10,7 @@ export const revalidate = 60
 export default async function ISRFSTestPage() {
   const now = new Date().toISOString().slice(11, 16)
   console.log(`[ISR-FS-TEST] Now: ${now}`)
+  console.log(`[ISR-FS-TEST] import.meta.url: ${import.meta.url}`)
   console.log(`[ISR-FS-TEST] process.cwd(): ${process.cwd()}`)
   const absPath = resolve(import.meta.url, './file.txt')
   console.log(`[ISR-FS-TEST] Abs path: ${absPath}`)
