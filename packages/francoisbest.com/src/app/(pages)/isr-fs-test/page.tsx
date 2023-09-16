87dde70c19cabceb7021305d72a8c9bff51f4b1d
Francois Best <github@francoisbest.com>
2023-09-16 12:45:43 +0200

    chore: Debug process.cwd()

--- packages/francoisbest.com/src/app/(pages)/isr-fs-test/page.tsx
+++ packages/francoisbest.com/src/app/(pages)/isr-fs-test/page.tsx
@@ -8,6 +8,7 @@ export const revalidate = 60
 
 export default async function ISRFSTestPage() {
   const now = new Date().toISOString().slice(11, 16)
+  console.log(`[ISR-FS-TEST] process.cwd(): ${process.cwd()}`)
   console.log(`[ISR-FS-TEST] Now: ${now}`)
   const filePath = path.resolve(
     process.cwd(),
