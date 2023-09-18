fd40bfa7d0773d16de3ef1bda92111fb4c504790
Francois Best <github@francoisbest.com>
2023-09-18 16:59:39 +0200

    chore: Make lambda serverless

--- packages/francoisbest.com/src/app/lambda/route.ts
+++ packages/francoisbest.com/src/app/lambda/route.ts
@@ -2,6 +2,8 @@ import { resolve } from 'lib/paths'
 import { NextResponse } from 'next/server'
 import fs from 'node:fs/promises'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
   const path = resolve(import.meta.url, '../../../package.json')
   const contents = await fs.readFile(path, 'utf8')
