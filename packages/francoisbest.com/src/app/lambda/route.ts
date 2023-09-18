e96bc688194a38212ac97cde3255add0cc545413
Francois Best <github@francoisbest.com>
2023-09-18 16:58:34 +0200

    chore: Testing reading in a lambda

---
+++ packages/francoisbest.com/src/app/lambda/route.ts
@@ -0,0 +1,9 @@
+import { resolve } from 'lib/paths'
+import { NextResponse } from 'next/server'
+import fs from 'node:fs/promises'
+
+export async function GET() {
+  const path = resolve(import.meta.url, '../../../package.json')
+  const contents = await fs.readFile(path, 'utf8')
+  return NextResponse.json({ contents })
+}
