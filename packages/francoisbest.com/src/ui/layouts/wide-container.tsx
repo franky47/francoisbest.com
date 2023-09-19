f0eaef53a476541eb2a819ea33bbebaaceab6c88
Francois Best <github@francoisbest.com>
2023-09-19 23:21:53 +0200

    style: Merge wide container classNames

--- packages/francoisbest.com/src/ui/layouts/wide-container.tsx
+++ packages/francoisbest.com/src/ui/layouts/wide-container.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { twMerge } from 'tailwind-merge'
 
 export const WideContainer: React.FC<React.ComponentProps<'section'>> = ({
-  className = '',
+  className,
   ...props
 }) => {
   return (
     <section
-      className={`mx-0 lg:-mx-16 p-0 max-w-full md:max-w-none lg:max-w-4xl ${className}`}
+      className={twMerge(
+        'mx-0 lg:-mx-16 p-0 max-w-full md:max-w-none lg:max-w-4xl',
+        className
+      )}
       {...props}
     />
   )
