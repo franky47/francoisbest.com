11f888a084029ffc9fb9e28ec487e4019040e57a
Francois Best <github@francoisbest.com>
2023-09-20 00:03:47 +0200

    style: Dark mode & overflow scroll

--- packages/francoisbest.com/src/app/(pages)/posts/(content)/2023/storing-react-state-in-the-url-with-nextjs/query-spy.tsx
+++ packages/francoisbest.com/src/app/(pages)/posts/(content)/2023/storing-react-state-in-the-url-with-nextjs/query-spy.tsx
@@ -42,7 +42,7 @@ export const QuerySpy: React.FC = () => {
       <pre
         aria-label="Querystring spy"
         aria-description="For browsers where the query is hard to see (eg: on mobile)"
-        className="text-sm bg-gray-50/50 dark:bg-gray-950 border dark:border-gray-800 dark:shadow-inner rounded !p-2 my-2"
+        className="text-sm bg-gray-50/50 dark:bg-gray-950 border dark:border-gray-800 dark:shadow-inner rounded !p-2 my-2 overflow-auto"
       >
         {search.size === 0 && (
           <span className="italic text-gray-500/50">{'<empty>'}</span>
@@ -50,9 +50,9 @@ export const QuerySpy: React.FC = () => {
         {Array.from(search.entries()).map(([key, value], i) => (
           <React.Fragment key={key + value + i}>
             <span className="text-gray-500">{i === 0 ? '?' : '&'}</span>
-            <span className="text-blue-500">{key}</span>
+            <span className="text-blue-600 dark:text-blue-400">{key}</span>
             <span className="text-current">=</span>
-            <span className="text-pink-500 dark:text-pink-400">
+            <span className="text-pink-600 dark:text-pink-400">
               {value.replaceAll(' ', '+')}
             </span>
           </React.Fragment>
