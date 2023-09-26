0b68d5a47ce781561af72b5c18b1a2e56aa77249
Francois Best <github@francoisbest.com>
2023-09-26 13:28:13 +0200

    chore: Make embeds available globally in MDX

--- packages/francoisbest.com/src/mdx-components.tsx
+++ packages/francoisbest.com/src/mdx-components.tsx
@@ -2,6 +2,8 @@ import type { MDXComponents } from 'mdx/types'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Note } from 'ui/components/note'
+import { GitHubRepo } from 'ui/embeds/github-repo'
+import { NpmPackage } from 'ui/embeds/npm-package'
 import { MdxPageFooter } from 'ui/layouts/mdx-page-footer'
 import { MdxPageHeader } from 'ui/layouts/mdx-page-header'
 import { WideContainer } from 'ui/layouts/wide-container'
@@ -13,6 +15,8 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     MdxPageHeader,
     MdxPageFooter,
     WideContainer,
+    NpmPackage,
+    GitHubRepo,
     Image,
     // @ts-expect-error - Required for remark-mdx-images to work
     img: Image,
