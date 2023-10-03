3b6768dff7202e29def4e90aa9e2d7b2d2f716f5
Francois Best <github@francoisbest.com>
2023-10-03 15:45:31 +0200

    fix: Switch to Next.js data cache

--- packages/francoisbest.com/src/app/api/isr/route.ts
+++ packages/francoisbest.com/src/app/api/isr/route.ts
@@ -1,7 +1,7 @@
 import { revalidateTag } from 'next/cache'
 import { NextRequest, NextResponse } from 'next/server'
 
-const ACCEPTED_TAGS = ['npm']
+const ACCEPTED_TAGS = ['npm', 'github']
 
 export async function GET(req: NextRequest) {
   const token = req.nextUrl.searchParams.get('token')
--- packages/francoisbest.com/src/lib/services/github.ts
+++ packages/francoisbest.com/src/lib/services/github.ts
@@ -1,5 +1,4 @@
 import { Octokit } from '@octokit/rest'
-import { cache } from 'react'
 import 'server-only'
 
 export type GitHubRepositoryData = {
@@ -18,18 +17,40 @@ const gh = new Octokit({
   auth: process.env.GITHUB_TOKEN,
 })
 
-export const fetchRepository = cache(async function fetchRepository(
+const fetchWithTags: typeof fetch = async function fetchWithTags(...args) {
+  args[1] = {
+    ...(args[1] ?? {}),
+    next: {
+      tags: ['github'],
+      revalidate: 86_400, // 24h
+    },
+  }
+  return fetch(...args)
+}
+
+export const fetchRepository = async function fetchRepository(
   slug: string
 ): Promise<GitHubRepositoryData> {
+  const request = {
+    fetch: fetchWithTags,
+  }
   try {
     const [owner, repo] = slug.split('/')
     const [{ data: repository }, prs] = await Promise.all([
-      gh.repos.get({ owner, repo }),
-      gh.pulls.list({ owner, repo }),
+      gh.repos.get({
+        owner,
+        repo,
+        request,
+      }),
+      gh.pulls.list({
+        owner,
+        repo,
+        request,
+      }),
     ])
     let version: string | undefined
     try {
-      const releases = await gh.repos.getLatestRelease({ owner, repo })
+      const releases = await gh.repos.getLatestRelease({ owner, repo, request })
       version = releases.data.tag_name.replace(/^v/, '')
     } catch {}
     const numPrs = prs.data.filter(pr => pr.state === 'open').length
@@ -47,4 +68,4 @@ export const fetchRepository = cache(async function fetchRepository(
   } catch (error) {
     throw new Error(`Failed to retrieve repo data for ${slug}: ${error}`)
   }
-})
+}
