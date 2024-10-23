139ab1c7615ee3aeefa546c24f54e869ba6b362f
Francois Best <github@francoisbest.com>
2024-10-23 11:33:04 +0200

    fix: Remove deprecated nuqs export

--- packages/francoisbest.com/src/app/(pages)/posts/(content)/2023/storing-react-state-in-the-url-with-nextjs/query-spy.tsx
+++ packages/francoisbest.com/src/app/(pages)/posts/(content)/2023/storing-react-state-in-the-url-with-nextjs/query-spy.tsx
@@ -2,32 +2,11 @@
 
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
-import { subscribeToQueryUpdates } from 'nuqs'
 import React from 'react'
 import { Button } from 'ui/components/buttons/button'
 
 export const QuerySpy: React.FC = () => {
-  const initialSearchParams = useSearchParams()
-  const [search, setSearch] = React.useState<URLSearchParams>(() => {
-    if (typeof location !== 'object') {
-      // SSR
-      const out = new URLSearchParams()
-      if (!initialSearchParams) {
-        return out
-      }
-      for (const [key, value] of initialSearchParams) {
-        out.set(key, value)
-      }
-      return out
-    } else {
-      return new URLSearchParams(location.search)
-    }
-  })
-
-  React.useLayoutEffect(
-    () => subscribeToQueryUpdates(({ search }) => setSearch(search)),
-    []
-  )
+  const searchParams = useSearchParams()
 
   return (
     <figure className="not-prose !my-0">
@@ -44,10 +23,10 @@ export const QuerySpy: React.FC = () => {
         aria-description="For browsers where the query is hard to see (eg: on mobile)"
         className="my-2 overflow-auto rounded border bg-gray-50/50 !p-2 text-sm dark:border-gray-800 dark:bg-gray-950 dark:shadow-inner"
       >
-        {search.size === 0 && (
+        {searchParams.size === 0 && (
           <span className="italic text-gray-500/50">{'<empty>'}</span>
         )}
-        {Array.from(search.entries()).map(([key, value], i) => (
+        {Array.from(searchParams.entries()).map(([key, value], i) => (
           <React.Fragment key={key + value + i}>
             <span className="text-gray-500">{i === 0 ? '?' : '&'}</span>
             <span className="text-blue-600 dark:text-blue-400">{key}</span>
