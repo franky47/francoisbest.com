31c065de87503f7bc6fb6bed2dd00ae11cb74a8a
François Best <github@francoisbest.com>
2025-11-30 11:23:06 +0000

    feat: add social links page

---
+++ packages/francoisbest.com/src/app/(pages)/links/page.tsx
@@ -0,0 +1,24 @@
+export default function Page() {
+  return (
+    <>
+      <h1>Links</h1>
+      <ul>
+        <li>
+          <a href="https://bsky.app/profile/francoisbest.com">Bluesky</a>
+        </li>
+        <li>
+          <a href="https://x.com/nuqs47ng">Twitter</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/francoisbest/">LinkedIn</a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/@47ng-dev">YouTube</a>
+        </li>
+        <li>
+          <a href="https://www.twitch.tv/fortysevenfx">Twitch</a>
+        </li>
+      </ul>
+    </>
+  )
+}
